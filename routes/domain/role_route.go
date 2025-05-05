4e44e2bd9c6ee34130dab66178bf900edb986cee
kenichi <kenichixie@gmail.com>
2025-05-05 18:02:05 +0700

    refactor(role_route): add permission middleware to role routes
    Enhance security by adding permission middleware to all role-related routes, ensuring that only authorized users can perform specific actions.

--- routes/domain/role_route.go
+++ routes/domain/role_route.go
@@ -9,11 +9,11 @@ import (
 func RoleRoute(route *gin.Engine, middleware *middleware.Middleware, roleController role.Controller) {
 	routes := route.Group(":domain_code/role", middleware.AuthHandle(), middleware.ValidateDomain())
 	{
-		routes.GET("", roleController.Index)
-		routes.GET("/:id", roleController.Show)
-		routes.POST("", roleController.Create)
-		routes.PATCH("/:id", roleController.Update)
-		routes.POST("/:id/add-permissions", roleController.AddPermissions)
-		routes.POST("/:id/delete-permissions", roleController.DeletePermissions)
+		routes.GET("", middleware.Permission(role.Permissions.Key, role.Permissions.Index), roleController.Index)
+		routes.GET("/:id", middleware.Permission(role.Permissions.Key, role.Permissions.Show), roleController.Show)
+		routes.POST("", middleware.Permission(role.Permissions.Key, role.Permissions.Create), roleController.Create)
+		routes.PATCH("/:id", middleware.Permission(role.Permissions.Key, role.Permissions.Update), roleController.Update)
+		routes.POST("/:id/add-permissions", middleware.Permission(role.Permissions.Key, role.Permissions.AddPermissions), roleController.AddPermissions)
+		routes.POST("/:id/delete-permissions", middleware.Permission(role.Permissions.Key, role.Permissions.DeletePermissions), roleController.DeletePermissions)
 	}
 }
